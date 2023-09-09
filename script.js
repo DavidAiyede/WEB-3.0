dd4831177908ba2403d2805b1fae29d98ceac6e5
DavidAiyede <davidaiyede27@gmail.com>
2023-09-09 20:18:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const mainMenu = document.querySelector(".mainMenu")
+const closeMenu = document.querySelector(".closeMenu")
+const openMenu = document.querySelector(".openMenu")
+const body = document.querySelector("body")
+
+
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click" ,close);
+
+
+function show(){
+    mainMenu.style.display ="flex";
+    mainMenu.style.top ="0";
+    body.style.backgroundColor = "rgb(193, 193, 197)"
+    body.style.transition = ".3s"
+}
+function close(){
+    mainMenu.style.top = "-100%"
+    body.style.backgroundColor = "white"
+    body.style.transition = ".3s"
+}
